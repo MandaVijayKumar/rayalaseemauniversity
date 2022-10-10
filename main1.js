3e876eb9b76838489db9526892179d5897502d3f
vijaykumarmanda <mandavijaykumar40@gmail.com>
2022-10-10 22:14:00 +0530

    naac1

---
+++ main1.js
@@ -0,0 +1,146 @@
+const i1 = document.querySelector('#item1')
+const i2 = document.querySelector('#item2')
+const i3 = document.querySelector('#item3')
+const i4 = document.querySelector('#item4')
+const i5 = document.querySelector('#item5')
+const i6 = document.querySelector('#item6')
+i1.style.color = 'red';
+i1.style.backgroundColor = 'bisque'; 
+i1.style.borderTopRightRadius = '20px';
+i1.style.borderBottomRightRadius = '20px';
+const a1 = document.querySelector('#a1');
+const a2 = document.querySelector('#a2');
+const a3 = document.querySelector('#a3');
+const a4 = document.querySelector('#a4');
+const a5 = document.querySelector('#a5');
+console.log(a3)
+a1.addEventListener('click', () => {
+    i1.style.backgroundColor = 'bisque'; 
+    i1.style.borderTopRightRadius = '20px';
+    i1.style.borderBottomRightRadius = '20px';
+    i2.style.backgroundColor = 'azure'; 
+    i2.style.borderTopRightRadius = '0px';
+    i2.style.borderBottomRightRadius = '0px';
+    i3.style.backgroundColor = 'azure'; 
+    i3.style.borderTopRightRadius = '0px';
+    i3.style.borderBottomRightRadius = '0px';
+    i4.style.backgroundColor = 'azure'; 
+    i4.style.borderTopRightRadius = '0px';
+    i4.style.borderBottomRightRadius = '0px';
+    i5.style.backgroundColor = 'azure'; 
+    i5.style.borderTopRightRadius = '0px';
+    i5.style.borderBottomRightRadius = '0px';
+    i6.style.backgroundColor = 'azure'; 
+    i6.style.borderTopRightRadius = '0px';
+    i6.style.borderBottomRightRadius = '0px';
+
+})
+i2.addEventListener('click', () => {
+    i2.style.backgroundColor = 'bisque'; 
+    i2.style.borderTopRightRadius = '20px';
+    i2.style.borderBottomRightRadius = '20px';
+    i1.style.backgroundColor = 'azure'; 
+    i1.style.borderTopRightRadius = '0px';
+    i1.style.borderBottomRightRadius = '0px';
+    i3.style.backgroundColor = 'azure'; 
+    i3.style.borderTopRightRadius = '0px';
+    i3.style.borderBottomRightRadius = '0px';
+    i4.style.backgroundColor = 'azure'; 
+    i4.style.borderTopRightRadius = '0px';
+    i4.style.borderBottomRightRadius = '0px';
+    i5.style.backgroundColor = 'azure'; 
+    i5.style.borderTopRightRadius = '0px';
+    i5.style.borderBottomRightRadius = '0px';
+    i6.style.backgroundColor = 'azure'; 
+    i6.style.borderTopRightRadius = '0px';
+    i6.style.borderBottomRightRadius = '0px';
+
+})
+a2.addEventListener('click', () => {
+    i3.style.backgroundColor = 'bisque'; 
+    i3.style.borderTopRightRadius = '20px';
+    i3.style.borderBottomRightRadius = '20px';
+    i1.style.backgroundColor = 'azure'; 
+    i1.style.borderTopRightRadius = '0px';
+    i1.style.borderBottomRightRadius = '0px';
+    i2.style.backgroundColor = 'azure'; 
+    i2.style.borderTopRightRadius = '0px';
+    i2.style.borderBottomRightRadius = '0px';
+    i4.style.backgroundColor = 'azure'; 
+    i4.style.borderTopRightRadius = '0px';
+    i4.style.borderBottomRightRadius = '0px';
+    i5.style.backgroundColor = 'azure'; 
+    i5.style.borderTopRightRadius = '0px';
+    i5.style.borderBottomRightRadius = '0px';
+    i6.style.backgroundColor = 'azure'; 
+    i6.style.borderTopRightRadius = '0px';
+    i6.style.borderBottomRightRadius = '0px';
+
+})
+a3.addEventListener('click', () => {
+    i4.style.backgroundColor = 'bisque'; 
+    i4.style.borderTopRightRadius = '20px';
+    i4.style.borderBottomRightRadius = '20px';
+    i1.style.backgroundColor = 'azure'; 
+    i1.style.borderTopRightRadius = '0px';
+    i1.style.borderBottomRightRadius = '0px';
+    i2.style.backgroundColor = 'azure'; 
+    i2.style.borderTopRightRadius = '0px';
+    i2.style.borderBottomRightRadius = '0px';
+    i3.style.backgroundColor = 'azure'; 
+    i3.style.borderTopRightRadius = '0px';
+    i3.style.borderBottomRightRadius = '0px';
+    i5.style.backgroundColor = 'azure'; 
+    i5.style.borderTopRightRadius = '0px';
+    i5.style.borderBottomRightRadius = '0px';
+    i6.style.backgroundColor = 'azure'; 
+    i6.style.borderTopRightRadius = '0px';
+    i6.style.borderBottomRightRadius = '0px';
+    
+
+})
+a4.addEventListener('click', () => {
+    i5.style.backgroundColor = 'bisque'; 
+    i5.style.borderTopRightRadius = '20px';
+    i5.style.borderBottomRightRadius = '20px';
+    i1.style.backgroundColor = 'azure'; 
+    i1.style.borderTopRightRadius = '0px';
+    i1.style.borderBottomRightRadius = '0px';
+    i2.style.backgroundColor = 'azure'; 
+    i2.style.borderTopRightRadius = '0px';
+    i2.style.borderBottomRightRadius = '0px';
+    i3.style.backgroundColor = 'azure'; 
+    i3.style.borderTopRightRadius = '0px';
+    i3.style.borderBottomRightRadius = '0px';
+    i4.style.backgroundColor = 'azure'; 
+    i4.style.borderTopRightRadius = '0px';
+    i4.style.borderBottomRightRadius = '0px';
+    i6.style.backgroundColor = 'azure'; 
+    i6.style.borderTopRightRadius = '0px';
+    i6.style.borderBottomRightRadius = '0px';
+    
+
+})
+a5.addEventListener('click', () => {
+    i6.style.backgroundColor = 'bisque'; 
+    i6.style.borderTopRightRadius = '20px';
+    i6.style.borderBottomRightRadius = '20px';
+    i1.style.backgroundColor = 'azure'; 
+    i1.style.borderTopRightRadius = '0px';
+    i1.style.borderBottomRightRadius = '0px';
+    i2.style.backgroundColor = 'azure'; 
+    i2.style.borderTopRightRadius = '0px';
+    i2.style.borderBottomRightRadius = '0px';
+    i3.style.backgroundColor = 'azure'; 
+    i3.style.borderTopRightRadius = '0px';
+    i3.style.borderBottomRightRadius = '0px';
+    i4.style.backgroundColor = 'azure'; 
+    i4.style.borderTopRightRadius = '0px';
+    i4.style.borderBottomRightRadius = '0px';
+    i5.style.backgroundColor = 'azure'; 
+    i5.style.borderTopRightRadius = '0px';
+    i5.style.borderBottomRightRadius = '0px';
+
+    
+
+})
